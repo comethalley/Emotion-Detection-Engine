c2121881501916500fc76e8579969ecbdb5c57f6
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2014-11-16 22:39:22 +0530

    Refactored main.js code, cleaner and more organized now

--- source/node/js/main.js
+++ source/node/js/main.js
@@ -1,69 +1,86 @@
-var secondsPerWindow = 20;
+$(document).ready(function() {
+    audioState++;
+    if (audioState == 2) {
+        init();
+    }
+});
 
-function init() {
-	var length = null;
-	printer = null;
+function init(config) {
 
-	$('#audio').on('loadedmetadata', function() {
-		length = this.seekable.end(0);
-	});
-
-	var d2 = [];
-	var data = [frames, d2];
-
-	var baseOptions = {
-		series: {
-			lines: {
-				show: true,
-				lineWidth: 1
-			}
-		},
-		yaxis: {
-			autoscaleMargin: 0.01,
-			ticks: 0,
+    //configuration options for main plot and overview
+    var secondsPerWindow = 20;
+    var baseOptions = {
+        series: {
+            lines: {
+                show: true,
+                lineWidth: 1
+            }
+        },
+        yaxis: {
+            autoscaleMargin: 0.01,
+            ticks: 0,
             panRange: false
-		},
-		xaxis: {
-			ticks: 0
-		},
-		colors: ['#057cb8', 'orange', 'red', 'green'],
+        },
+        xaxis: {
+            ticks: 0
+        },
+        colors: ['#057cb8', 'orange', 'red', 'green'],
         grid: {
             hoverable: true,
             autoHighlight: false
         }
-	};
+    };
 
-	var mainPlotOptions = $.extend(true, {}, baseOptions, {
-		xaxis: {
-			min: 0,
-			max: 10 * secondsPerWindow
-		},
+    var mainPlotOptions = $.extend(true, {}, baseOptions, {
+        xaxis: {
+            min: 0,
+            max: 10 * secondsPerWindow
+        },
         pan: {
             interactive: true
         }
-	});
+    });
 
     var overviewPlotOptions = $.extend(true, {}, baseOptions, {
         selection: {
-			mode: "x",
-			color: '#057cb8'
-		}
+            mode: "x",
+            color: '#057cb8'
+        }
     });
 
-	plot = $.plot("#placeholder", data, mainPlotOptions);
-	overview = $.plot('#overview', data, overviewPlotOptions);
+    //state and data variables
+    var streamed = false;
+    var data = [frames, []];
+    var audio = document.getElementById('audio');
 
-    var mainPlaceholder = $('#placeholder');
-	var overviewPlaceholder = $('#overview');
+    //jQuery elements for containers
+    var plotContainer = $('#placeholder');
+    var overviewContainer = $('#overview');
 
-    overviewPlaceholder.on('plotselected', function(e, ranges) {
-        plot = $.plot('#placeholder', data,
+    //Flot objects for plots
+    var plot = $.plot(plotContainer, data, mainPlotOptions);
+    var overview = $.plot(overviewContainer, data, overviewPlotOptions);
+
+    //main timed loop for replotting
+    var prevTime = 0;
+    window.setInterval(function () {
+        var currentTime = audio.currentTime;
+        if (currentTime != prevTime) {
+            //pass elapsed fraction of audio play to replot()
+            replot(currentTime/audio.seekable.end(0));
+            prevTime = currentTime;
+        }
+    }, 100);
+
+    //event handlers on plot containers
+    overviewContainer.on('plotselected', function(e, ranges) {
+        plot = $.plot(plotContainer, data,
             $.extend(true, {}, mainPlotOptions, {
              xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to }
         }));
     });
 
-    mainPlaceholder.on('plotpan', function(e, plot, args) {
+    plotContainer.on('plotpan', function(e, plot, args) {
         var axes = plot.getAxes();
         var ranges = {
             xaxis: {
@@ -71,43 +88,24 @@ function init() {
                 to: axes.xaxis.max
             }
         }
-        overview.setSelection(ranges, true)
+        overview.setSelection(ranges, true);
     });
 
-	function replot(elapsedFraction) {
+    plotContainer.on('dblclick', function() {
+        var xaxis = plot.getAxes().xaxis;
+        var xmin = xaxis.datamin;
+        var xmax = xaxis.datamax;
+        plot = $.plot(plotContainer, data,
+            $.extend(true, {}, mainPlotOptions, {
+             xaxis: { min: xmin, max: xmax }
+        }));
+        overview = $.plot(overviewContainer, data,
+            $.extend(true, {}, overviewPlotOptions, {
+             xaxis: { min: xmin, max: xmax }
+        }));
+    });
 
-		var axes = plot.getAxes();
-		var xmin = axes.xaxis.datamin;
-		var xmax = axes.xaxis.datamax;
-		var ymin = axes.yaxis.datamin;
-		var ymax = axes.yaxis.datamax;
-
-		data[0] = frames.slice(0, elapsedFraction * frames.length);
-
-		data[1] = [[(xmax - xmin), ymin], [(xmax - xmin), ymax]];
-
-		plot.setData(data);
-        overview.setData(data);
-        plot.getData()[1].lines.lineWidth = 3
-
-		plot.setupGrid();
-        overview.setupGrid();
-        overview.draw();
-		plot.draw();
-	}
-
-	replot(0);
-	var prevTime = 0;
-
-	window.setInterval(function () {
-		var audio = $('#audio').get(0);
-		var currentTime = audio.currentTime;
-		if (currentTime != prevTime) {
-			replot(currentTime/audio.seekable.end(0));
-			prevTime = currentTime;
-		}
-	}, 100);
-
+    //play selection part button event handler
     $('#playpart').on('click', function() {
         $('#audio').on('timeupdate', function(e) {
             var startFraction = (axes.xaxis.max - axes.xaxis.datamin)/(axes.xaxis.datamax - axes.xaxis.datamin);
@@ -126,24 +124,27 @@ function init() {
         audio.play();
     });
 
-    $('#placeholder').on('dblclick', function() {
-        var xaxis = plot.getAxes().xaxis;
-		var xmin = xaxis.datamin;
-		var xmax = xaxis.datamax;
-		plot = $.plot('#placeholder', data,
-            $.extend(true, {}, mainPlotOptions, {
-             xaxis: { min: xmin, max: xmax }
-        }));
-        overview = $.plot('#overview', data,
-            $.extend(true, {}, overviewPlotOptions, {
-             xaxis: { min: xmin, max: xmax }
-        }));
-    });
-}
+    function replot(elapsedFraction) {
 
-$(document).ready(function() {
-	audioState++;
-	if (audioState == 2) {
-		init();
-	}
-});
+        var axes = plot.getAxes();
+        var xmin = axes.xaxis.datamin;
+        var xmax = axes.xaxis.datamax;
+        var ymin = axes.yaxis.datamin;
+        var ymax = axes.yaxis.datamax;
+
+        data[0] = frames.slice(0, elapsedFraction * frames.length);
+
+        data[1] = [[(xmax - xmin), ymin], [(xmax - xmin), ymax]];
+
+        plot.setData(data);
+        plot.getData()[1].lines.lineWidth = 3
+        overview.setData(data);
+
+        plot.setupGrid();
+        plot.draw();
+        overview.setupGrid();
+        overview.draw();
+    }
+
+    replot(0);
+}
